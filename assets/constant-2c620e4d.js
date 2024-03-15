5043cf5cbf0523fe9b4a2470c0c5de6887e88f30
ChenJW <2774721329@qq.com>
2024-03-15 17:26:06 +0800

    feat: 更新V1.3.1示例代码

---
+++ assets/constant-2c620e4d.js
@@ -0,0 +1,280 @@
+const l=""+new URL("000703-16738852232359-e123e316.webp",import.meta.url).href,n=""+new URL("000732-16489156528a86-62fdbfe6.webp",import.meta.url).href,p=""+new URL("000831-1648915711b98a-67f707dc.webp",import.meta.url).href,c=""+new URL("001611-1596212171d777-de6f0d07.webp",import.meta.url).href,m=""+new URL("002213-167379973369d6-20220e89.webp",import.meta.url).href,g=""+new URL("002407-16735406474623-04954e89.webp",import.meta.url).href,_=""+new URL("002815-16740592954afa-6e6b986b.webp",import.meta.url).href,b=""+new URL("002828-1654792108762f-b24e7a46.webp",import.meta.url).href,d=""+new URL("004156-1675701716a172-12285932.webp",import.meta.url).href,w=""+new URL("004307-16757017873bc6-2865571f.webp",import.meta.url).href,u=""+new URL("004413-16757018539a1b-5a747511.webp",import.meta.url).href,h=""+new URL("004827-1673110107646e-1141edac.webp",import.meta.url).href,f=""+new URL("005104-16730238642321-2e57ae65.webp",import.meta.url).href,v=""+new URL("005245-167319676525cb-f9d598dd.webp",import.meta.url).href,L=""+new URL("005741-167432026152b8-bbd27c98.webp",import.meta.url).href,j=""+new URL("005836-16499555168372-be10cb54.webp",import.meta.url).href,y=""+new URL("113340-1650944020d166-3f215b47.webp",import.meta.url).href,R=""+new URL("153854-16578707341b13-ccbddad5.webp",import.meta.url).href,U=""+new URL("154100-1674114060d02c-f1dbb985.webp",import.meta.url).href,k=""+new URL("154242-1674114162e7d7-f2b35422.webp",import.meta.url).href,P=""+new URL("154624-16741143848abd-0b508322.webp",import.meta.url).href,W=""+new URL("165158-16727359185812-01648184.webp",import.meta.url).href,T=""+new URL("165525-167273612558a6-d075a715.webp",import.meta.url).href,C=""+new URL("172210-16742065302a9c-9be0d70e.webp",import.meta.url).href,S=""+new URL("173740-16722202608132-329e1ae4.webp",import.meta.url).href,x=""+new URL("173820-164189390070b9-fcce4ce5.webp",import.meta.url).href,I=""+new URL("184041-1674556841997c-2d36a0c5.webp",import.meta.url).href,D=""+new URL("192110-1568028070e1cc-6818bbf4.webp",import.meta.url).href,q=""+new URL("200340-164233462027dc-636c7b06.webp",import.meta.url).href,E=""+new URL("203400-166791084068b8-50307f12.webp",import.meta.url).href,M=""+new URL("215849-16651511295207-7760e70b.webp",import.meta.url).href,O=""+new URL("220151-16534873114c8d-98ca6ff4.webp",import.meta.url).href,Z=""+new URL("220501-1586700301dfe4-f793e7e7.webp",import.meta.url).href,B=""+new URL("233139-167371029908dd-4c0c2ec0.webp",import.meta.url).href,H=""+new URL("60824-5074db22.jpg",import.meta.url).href,z=""+new URL("60825-4e767d73.jpg",import.meta.url).href,G=""+new URL("60826-334d2ce0.jpg",import.meta.url).href,N=""+new URL("60827-2574d7a8.jpg",import.meta.url).href,V=""+new URL("60828-ef417789.jpg",import.meta.url).href,$=""+new URL("60829-06205886.jpg",import.meta.url).href,A=""+new URL("60830-4fff7461.jpg",import.meta.url).href,K=""+new URL("60831-8df110da.jpg",import.meta.url).href,F=""+new URL("60832-0b48e1d4.jpg",import.meta.url).href,J=""+new URL("60833-2ad8f205.jpg",import.meta.url).href,Q=""+new URL("60835-d6ed8652.jpg",import.meta.url).href,X=""+new URL("60836-c2ca47c4.jpg",import.meta.url).href,Y=""+new URL("60838-6becef3f.jpg",import.meta.url).href,ee=""+new URL("60839-3bc1f898.jpg",import.meta.url).href,te=""+new URL("63531-17ef6378.jpg",import.meta.url).href,se=""+new URL("63532-e95111bd.jpg",import.meta.url).href,ie=""+new URL("63533-691f34cb.jpg",import.meta.url).href,ae=""+new URL("63609-6cdeafff.jpg",import.meta.url).href,oe=""+new URL("63610-dca39e92.jpg",import.meta.url).href,re=""+new URL("63611-c08ec613.jpg",import.meta.url).href,le=""+new URL("read-book-e8460b72.jpg",import.meta.url).href,ne=""+new URL("red-silk-6a8802d7.jpg",import.meta.url).href,pe=""+new URL("red-silk2-9745d8d7.webp",import.meta.url).href,ce=""+new URL("white-lotus-a4ab036f.webp",import.meta.url).href,me=""+new URL("white-silk-3fe88081.webp",import.meta.url).href,ge={isNetworkImageLoose:{pattern:/^(http:\/\/|https:\/\/).*$/,msg:"不是网络图片，不以http://或https://开头"},webglTransitionParent:{pattern:/^(webgl-transition-).*$/}},e=s=>new URL(Object.assign({"../assets/images/000703-16738852232359.webp":l,"../assets/images/000732-16489156528a86.webp":n,"../assets/images/000831-1648915711b98a.webp":p,"../assets/images/001611-1596212171d777.webp":c,"../assets/images/002213-167379973369d6.webp":m,"../assets/images/002407-16735406474623.webp":g,"../assets/images/002815-16740592954afa.webp":_,"../assets/images/002828-1654792108762f.webp":b,"../assets/images/004156-1675701716a172.webp":d,"../assets/images/004307-16757017873bc6.webp":w,"../assets/images/004413-16757018539a1b.webp":u,"../assets/images/004827-1673110107646e.webp":h,"../assets/images/005104-16730238642321.webp":f,"../assets/images/005245-167319676525cb.webp":v,"../assets/images/005741-167432026152b8.webp":L,"../assets/images/005836-16499555168372.webp":j,"../assets/images/113340-1650944020d166.webp":y,"../assets/images/153854-16578707341b13.webp":R,"../assets/images/154100-1674114060d02c.webp":U,"../assets/images/154242-1674114162e7d7.webp":k,"../assets/images/154624-16741143848abd.webp":P,"../assets/images/165158-16727359185812.webp":W,"../assets/images/165525-167273612558a6.webp":T,"../assets/images/172210-16742065302a9c.webp":C,"../assets/images/173740-16722202608132.webp":S,"../assets/images/173820-164189390070b9.webp":x,"../assets/images/184041-1674556841997c.webp":I,"../assets/images/192110-1568028070e1cc.webp":D,"../assets/images/200340-164233462027dc.webp":q,"../assets/images/203400-166791084068b8.webp":E,"../assets/images/215849-16651511295207.webp":M,"../assets/images/220151-16534873114c8d.webp":O,"../assets/images/220501-1586700301dfe4.webp":Z,"../assets/images/233139-167371029908dd.webp":B,"../assets/images/60824.jpg":H,"../assets/images/60825.jpg":z,"../assets/images/60826.jpg":G,"../assets/images/60827.jpg":N,"../assets/images/60828.jpg":V,"../assets/images/60829.jpg":$,"../assets/images/60830.jpg":A,"../assets/images/60831.jpg":K,"../assets/images/60832.jpg":F,"../assets/images/60833.jpg":J,"../assets/images/60835.jpg":Q,"../assets/images/60836.jpg":X,"../assets/images/60838.jpg":Y,"../assets/images/60839.jpg":ee,"../assets/images/63531.jpg":te,"../assets/images/63532.jpg":se,"../assets/images/63533.jpg":ie,"../assets/images/63609.jpg":ae,"../assets/images/63610.jpg":oe,"../assets/images/63611.jpg":re,"../assets/images/read-book.jpg":le,"../assets/images/red-silk.jpg":ne,"../assets/images/red-silk2.webp":pe,"../assets/images/white-lotus.webp":ce,"../assets/images/white-silk.webp":me})[`../assets/images/${s}`],self.location).href,_e=s=>{let i=[];for(let t=0;t<s.length;t++){let o=new Promise((r,be)=>{let a=new Image;ge.isNetworkImageLoose.pattern.test(s[t])&&a.setAttribute("crossOrigin","Anonymous"),a.onload=function(){r(a)},a.src=s[t]});i.push(o)}return Promise.all(i).then(t=>(console.log("loaded all images"),Promise.resolve(t)))},de=async(s,i)=>{try{let t=await _e(s);i(t)}catch(t){console.log(t),i([])}},we=[{path:"",title:"wind",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"wind",content:"..."},{path:"",title:"waterDrop",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"waterDrop",content:"..."},{path:"",title:"squaresWire",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"squaresWire",content:"..."},{path:"",title:"crossWarp",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"crossWarp",content:"..."},{path:"",title:"crossZoom",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"crossZoom",content:"..."},{path:"",title:"directionalWarp",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"directionalWarp",content:"..."},{path:"",title:"dreamy",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"dreamy",content:"..."},{path:"",title:"flyEye",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"flyEye",content:"..."},{path:"",title:"morph",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"morph",content:"..."},{path:"",title:"mosaic",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"mosaic",content:"..."},{path:"",title:"perlin",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"perlin",content:"..."},{path:"",title:"randomSquares",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"randomSquares",content:"..."},{path:"",title:"ripple",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"ripple",content:"..."},{path:"",title:"simpleZoom",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"simpleZoom",content:"..."},{path:"",title:"directional",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"directional",content:"..."},{path:"",title:"windowSlice",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"windowSlice",content:"..."},{path:"",title:"invertedPageCurl",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"invertedPageCurl",content:"..."},{path:"",title:"linearBlur",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"linearBlur",content:"..."},{path:"",title:"glitchMemories",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"glitchMemories",content:"..."},{path:"",title:"polkaDotsCurtain",playPicList:[e("red-silk.jpg"),e("red-silk2.webp")],description:"polkaDotsCurtain",content:"..."}],ue=["wind","waterDrop","squaresWire","dreamy","crossWarp","crossZoom","directionalWarp","randomSquares","ripple","flyEye","morph","mosaic","perlin","simpleZoom","directional","windowSlice","linearBlur","invertedPageCurl","glitchMemories","polkaDotsCurtain"],he=`<script setup lang="ts" name="mixedExample">
+import { computed } from "@vue/runtime-core";
+import { onMounted } from "vue";
+import {
+    WebglTransitions,
+    wind,
+    waterDrop,
+    squaresWire,
+    crossWarp,
+    crossZoom,
+    directionalWarp,
+    dreamy,
+    flyEye,
+    morph,
+    mosaic,
+    perlin,
+    randomSquares,
+    ripple,
+    simpleZoom,
+    directional,
+    windowSlice,
+    invertedPageCurl,
+    linearBlur,
+    glitchMemories,
+    polkaDotsCurtain,
+} from "webgl-transition";
+import { debounce } from "@/tools/common";
+
+let webglTransitions: WebglTransitions;
+const glcanvasRef = ref<HTMLDivElement>();
+const clientWidth = ref(0);
+const clientHeight = ref(0);
+const calcWidth = () => {
+    clientWidth.value = glcanvasRef.value ? glcanvasRef.value.clientWidth : 0;
+};
+const calcHeight = () => {
+clientHeight.value = glcanvasRef.value
+    ? Number((glcanvasRef.value?.clientWidth * 2400) / 3600)
+    : 0;
+};
+onMounted(() => {
+    calcWidth();
+    calcHeight();
+    nextTick(() => {
+        const imgsBase = "https://images.pexels.com/photos/";
+        const imgs = [
+            'https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg',
+            'https://images.pexels.com/photos/236660/pexels-photo-236660.jpeg',
+        ];
+        const obj = {
+        parentId: "#glcanvas",
+        transitionList: [
+            wind,
+            waterDrop,
+            squaresWire,
+            crossWarp,
+            crossZoom,
+            directionalWarp,
+            dreamy,
+            flyEye,
+            morph,
+            mosaic,
+            perlin,
+            randomSquares,
+            ripple,
+            simpleZoom,
+            directional,
+            windowSlice,
+            invertedPageCurl,
+            linearBlur,
+            glitchMemories,
+            polkaDotsCurtain,
+        ],
+        playPicUrlList: imgs,
+        playPicList: [],
+        carouselTime: 1000,
+        watchResize: false,
+        };
+        webglTransitions = new WebglTransitions(obj);
+        webglTransitions?.main();
+
+        const resizeObserver = new ResizeObserver(
+        debounce(() => {
+            calcWidth();
+            calcHeight();
+            webglTransitions?.onResize({
+            width: clientWidth.value,
+            height: clientHeight.value,
+            });
+        }, 300)
+        );
+        glcanvasRef.value && resizeObserver.observe(glcanvasRef.value);
+    });
+});
+const lost = () => {
+    webglTransitions?.simulatedLostContext();
+};
+const stop = () => {
+    webglTransitions?.stop();
+    webglTransitions?.dispose();
+};
+
+// clear Interval when page unmounted
+onUnmounted(() => {
+    stop();
+});
+<\/script>
+
+<template>
+<div>
+    <div
+    ref="glcanvasRef"
+    id="glcanvas"
+    :style="{ width: '100vw', height: clientHeight + 'px' }"
+    ></div>
+</div>
+</template>
+
+<style scoped lang="less">
+#glcanvas {
+    overflow: hidden;
+}
+</style>
+`,fe=`<script setup lang="ts" name="separateExample">
+import { ref } from "vue";
+
+import { GridItem } from "../interface";
+import { singleWebglTransitionList } from "../constant";
+import gridLayout from "@/components/grid-layout.vue";
+import {
+  WebglTransitions,
+  wind,
+  waterDrop,
+  squaresWire,
+  crossWarp,
+  crossZoom,
+  directionalWarp,
+  dreamy,
+  flyEye,
+  morph,
+  mosaic,
+  perlin,
+  randomSquares,
+  ripple,
+  simpleZoom,
+  directional,
+  windowSlice,
+  invertedPageCurl,
+  linearBlur,
+  glitchMemories,
+  polkaDotsCurtain,
+} from "webgl-transition";
+
+import RULE from "../../tools/rules";
+import { asyncLoadImage } from "../utils";
+
+interface ObjectKey {
+  [key: string]: any;
+}
+
+const transitionObject: ObjectKey = {
+  wind: wind,
+  waterDrop: waterDrop,
+  squaresWire: squaresWire,
+  crossWarp: crossWarp,
+  crossZoom: crossZoom,
+  directionalWarp: directionalWarp,
+  dreamy: dreamy,
+  flyEye: flyEye,
+  morph: morph,
+  mosaic: mosaic,
+  perlin: perlin,
+  randomSquares: randomSquares,
+  ripple: ripple,
+  simpleZoom: simpleZoom,
+  directional: directional,
+  windowSlice: windowSlice,
+  invertedPageCurl: invertedPageCurl,
+  linearBlur: linearBlur,
+  glitchMemories: glitchMemories,
+  polkaDotsCurtain: polkaDotsCurtain,
+};
+
+const gridLayoutRef = ref<typeof gridLayout>();
+const router = useRouter();
+let listData = singleWebglTransitionList.map((o: GridItem, i: number) => {
+  o.id = 'webgl-transition-parent-Math.random().toString().slice(2, 10)';
+  return o;
+});
+const moduleTitle = "webgl-transition demo(V1.3.1)";
+const imgsBase = "https://images.pexels.com/photos/";
+const onlineImages = [
+    'https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg',
+    'https://images.pexels.com/photos/236660/pexels-photo-236660.jpeg',
+];
+let webglTransitions: WebglTransitions;
+const lastPlayObject = ref<GridItem>();
+
+/**
+ * function
+ */
+const onClickGrid = async (object: GridItem) => {
+  if (
+    webglTransitions?.playStatus === "pause" &&
+    lastPlayObject.value?.id === object.id
+  ) {
+    webglTransitions?.continue();
+  } else if (
+    webglTransitions?.playStatus === "stop" ||
+    lastPlayObject.value?.id != object.id
+  ) {
+    if (lastPlayObject.value) {
+      const el = document.querySelector('#lastPlayObject.value?.id');
+      if (el) {
+        const childs = el.children;
+        for (let i = childs.length - 1; i >= 0; i--) {
+          if (RULE.webglTransitionParent.pattern.test(childs[i].id)) {
+            el.removeChild(childs[i]);
+          }
+        }
+      }
+    }
+    webglTransitions?.stop();
+    webglTransitions?.dispose();
+    asyncLoadImage(object.playPicList, (localImages: HTMLImageElement[]) => {
+      const obj = {
+        parentId: '#object.id',
+        transitionList: [transitionObject[object.title]],
+        playPicUrlList: [...onlineImages],
+        playPicList: localImages,
+        watchResize: true,
+      };
+      webglTransitions = new WebglTransitions(obj);
+      webglTransitions?.main();
+      lastPlayObject.value = object;
+    });
+  } else if (
+    webglTransitions?.playStatus === "playing" &&
+    lastPlayObject.value?.id === object.id
+  ) {
+    webglTransitions?.pause();
+  }
+};
+
+const onViewCode = () => {
+  router.push("/view-code");
+};
+
+// clear Interval when page unmounted
+onUnmounted(() => {
+  webglTransitions?.stop();
+  webglTransitions?.dispose();
+});
+<\/script>
+
+<template>
+  <div class="separate-example-container">
+    <h1 class="main-title">{{ moduleTitle }}</h1>
+
+    <h3>Separate example</h3>
+    <p>
+      Each is a separate instance showing a different transition animation
+      effect.
+      <a @click="onViewCode" class="cursor">(documentation)</a>
+    </p>
+    <grid-layout
+      ref="gridLayoutRef"
+      :title="moduleTitle"
+      :list="listData"
+      @on-click-grid="onClickGrid"
+    ></grid-layout>
+  </div>
+</template>
+
+<style scoped lang="less">
+.separate-example-container {
+    color: #888;
+}
+</style>
+`;export{ge as R,de as a,ue as b,fe as c,we as l,he as s};
